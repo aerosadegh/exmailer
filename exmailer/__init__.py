0aa2c12b59303a86725552a93ce2b142fe8abeeb
Sadegh Yazdani <m.s.yazdani86@gmail.com>
2026-02-08 09:24:29 +0330

    fix: improve import pkg from init

--- exmailer/__init__.py
+++ exmailer/__init__.py
@@ -5,7 +5,7 @@ from .exceptions import (
     ConfigurationError,
     ExchangeEmailConnectionError,
 )
-from .templates import get_default_template, get_persian_template
+from .templates import get_default_template, get_persian_template, register_custom_template
 
 __version__ = "1.0.0"
 __author__ = "Sadegh Yazdani"
@@ -13,6 +13,7 @@ __all__ = [
     "ExchangeEmailer",
     "get_default_template",
     "get_persian_template",
+    "register_custom_template",
     "AuthenticationError",
     "ExchangeEmailConnectionError",
     "AttachmentError",
