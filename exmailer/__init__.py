7edb6765e5f77e74755cb0858b32d369100050d8
Sadegh Yazdani <m.s.yazdani86@gmail.com>
2026-02-08 14:24:11 +0330

    chore: bump version to v1.0.2

--- exmailer/__init__.py
+++ exmailer/__init__.py
@@ -7,7 +7,7 @@ from .exceptions import (
 )
 from .templates import get_default_template, get_persian_template, register_custom_template
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "Sadegh Yazdani"
 __all__ = [
     "ExchangeEmailer",
